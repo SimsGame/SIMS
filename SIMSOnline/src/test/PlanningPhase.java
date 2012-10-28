adf1fe37a33e453dee938b08991de554e5238d0b
Tobi <Tobi@141.72.112.174>
2012-10-28 21:02:09 +0100

    Planningphase: StudInfo.java implementiert, die die aktuellen Attribute eines Student anzeigt

--- SIMSOnline/src/test/PlanningPhase.java
+++ SIMSOnline/src/test/PlanningPhase.java
@@ -6,9 +6,46 @@ package test;
 
 /**
  * 
- * @author Tobi
+ * @author Tobias Mauritz
  * @author Yuliya
  */
 public class PlanningPhase {
+//     double id;
+//     double intelligence;
+//     double knowledge;
+//     double motivation;
+//     double tiredness;
+//     static StudentArray StudArr;
+    
+    public PlanningPhase(){
+       
+//        StudArr = new StudentArray();
+//        
+//        this.id = StudArr.getArray()[2].getId();
+//        this.intelligence = StudArr.getArray()[2].getIntelligence();
+//        this.knowledge = StudArr.getArray()[2].getKnowledge();
+//        this.motivation = StudArr.getArray()[2].getMotivation();
+//        this.tiredness = StudArr.getArray()[2].getTiredness();
+        PlanningPhaseMain();
+    
+    }
+    
+//    public PlanningPhase(javax.swing.JProgressBar jProgB_Knowledge, 
+//                    javax.swing.JProgressBar jProgB_Motivation, 
+//                    javax.swing.JProgressBar jProgB_Tiredness){
+//        
+//        StudInfo x = new StudInfo(jProgB_Knowledge,jProgB_Motivation, jProgB_Tiredness);
+//    }
+    
+    private void PlanningPhaseMain(){
+//        System.out.println("id = "+id);
+//        System.out.println("id = "+intelligence);
+//        System.out.println("id = "+knowledge);
+//        System.out.println("id = "+motivation);
+//        System.out.println("id = "+tiredness);
+        
+
+    }
+    
     
 }
