007818d34a73e2efce8470c442bf00f33457b1c5
nadir35 <nadir35@IBM-SQRGSVNK52N>
2012-10-29 11:07:56 +0100

    ask nadir

--- SIMSOnline/src/test/UpdateBars.java
+++ SIMSOnline/src/test/UpdateBars.java
@@ -6,27 +6,51 @@ package test;
 
 /**
  *
- * @author Nadir Yuldashev
+ * @author Nadir Yuldashev, Jörg Woditschka
  */
 public class UpdateBars extends Thread {
 
-    private int knowledge=0;
-    private javax.swing.JProgressBar KnowledgeBar;
-    private int timer = Timer.timer;
-
-   public UpdateBars(javax.swing.JProgressBar jKnowledgeBar) {
-        this.KnowledgeBar = jKnowledgeBar;
-        
    
+    private Game game;
+    private double avrgmotivation;
+    private double avrgtiredness;
+    private double avrgknowledge;
+    private javax.swing.JProgressBar KnowledgeBar;
+    private javax.swing.JProgressBar MotivationBar;
+    private javax.swing.JProgressBar TirednessBar;
+    private int timer = 10;
+
+   public UpdateBars(javax.swing.JProgressBar jKnowledgeBar,javax.swing.JProgressBar jMotivationBar,javax.swing.JProgressBar jTirednessBar, Game game) {
+        this.KnowledgeBar = jKnowledgeBar;
+        this.MotivationBar = jMotivationBar;
+        this.TirednessBar = jTirednessBar;
+        this.game = game;
     }
 
+   private void updateAvrg(){
+       for(int i=0;i<30;i++){
+           avrgknowledge+=game.studentArray[i].getKnowledge();
+           avrgmotivation+=game.studentArray[i].getMotivation();
+           avrgtiredness+=game.studentArray[i].getTiredness();
+       }
+       avrgknowledge/=30;
+       avrgmotivation/=30;
+       avrgtiredness/=30;
+       
+       System.out.println("motivation"+ avrgmotivation + "   "+ "Tiredness" +avrgtiredness +"Knowledge" +avrgknowledge);
+   }
+   
     @Override
     public void run() {
         while(timer>0){
-            knowledge=knowledge+1;
-         KnowledgeBar.setValue(knowledge); 
+             updateAvrg();
+         KnowledgeBar.setValue((int)(avrgknowledge*250)); 
+         MotivationBar.setValue((int)avrgmotivation); 
+         TirednessBar.setValue((int)avrgtiredness); 
          KnowledgeBar.repaint();
-        try{Thread.sleep(50);} //Sleep 50 milliseconds
+         MotivationBar.repaint();
+         TirednessBar.repaint();
+        try{Thread.sleep(0);} //Sleep 50 milliseconds
                 catch (InterruptedException err){}
 }
 
