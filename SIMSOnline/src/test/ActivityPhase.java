691668fa74a380113232e26f5ea79799aecb0b3d
nadir35 <nadir35@IBM-SQRGSVNK52N>
2012-10-29 11:07:09 +0100

    update activityphase bars

--- SIMSOnline/src/test/ActivityPhase.java
+++ SIMSOnline/src/test/ActivityPhase.java
@@ -13,16 +13,24 @@ public class ActivityPhase {
 
     private javax.swing.JLabel label_timer;
     private javax.swing.JProgressBar KnowledgeBar;
+    private javax.swing.JProgressBar MotivationBar;
+    private javax.swing.JProgressBar TirednessBar;
+    private Game game;
 
-    public ActivityPhase(javax.swing.JLabel jLabel_timer, javax.swing.JProgressBar jKnowledgeBar) {
+    public ActivityPhase(javax.swing.JLabel jLabel_timer, javax.swing.JProgressBar jKnowledgeBar,javax.swing.JProgressBar jMotivationBar,javax.swing.JProgressBar jTirednessBar
+) {
         this.label_timer = jLabel_timer;
         this.KnowledgeBar = jKnowledgeBar;
+        this.MotivationBar = jMotivationBar;
+        this.TirednessBar = jTirednessBar;
+        game = new Game();
+        game.initArray();
         activityPhaseMain();
     }
     
     private void activityPhaseMain(){
-        Thread runTimer = new Thread(new Timer(label_timer));
-         Thread updateBars = new Thread(new UpdateBars(KnowledgeBar));
+        Thread runTimer = new Thread(new Timer(label_timer, game));
+        Thread updateBars = new Thread(new UpdateBars(KnowledgeBar, MotivationBar,TirednessBar, game));
         runTimer.start();
         updateBars.start();
     }
