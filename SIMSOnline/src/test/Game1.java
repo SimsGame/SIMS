389a2592a0ca314161b50db66d55647cd2ec0259
rusinda <rusinda@RUSINDA1.emea.hpqcorp.net>
2012-11-09 15:52:16 +0100

    Revert "loadGame()"
    This reverts commit c092ee3196d46354a16fca3b7ec194389a696322.

--- SIMSOnline/src/test/Game1.java
+++ SIMSOnline/src/test/Game1.java
@@ -227,6 +227,7 @@ public class Game1 {
         help = inventory.pop();
         putItem(Item._omniSenseName, new Integer(help.get(0)), new Integer(help.get(1)));
         
+        JOptionPane.showMessageDialog(null, Arrays.toString(this.studentArray));
     }
     
 /**
