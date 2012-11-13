7ed7d5ab9dd4edba747577377d287f174c72bd43
nadir35 <nadir35@nadir35-HP.localdomain>
2012-11-13 23:14:28 +0100

    merge..

--- SIMSOnline/src/test/Game1.java
+++ SIMSOnline/src/test/Game1.java
@@ -329,7 +329,7 @@ public class Game1 {
         if (this.barNum == 0) {
             for (int i = 0; i < 30; i++) {
                 if (this.studentArray[i].present) {
-                    Color color = new Color(220, 220, 0);
+                    Color color = new Color(220, 220, 220);
                     studButtons[i].setBackground(color);
                     studButtons[i].setOpaque(true);
                 }
@@ -374,8 +374,8 @@ public class Game1 {
                     if (knowledge > 255) {
                         knowledge = 255;
                     }
-                    //Color color = new Color(255-knowledge, knowledge, 0);
-                    //studButtons[i].setBackground(color);
+                    Color color = new Color(255-knowledge, knowledge, 0);
+                    studButtons[i].setBackground(color);
                     studButtons[i].setOpaque(true);
                 }
             }
